e0d87fea5805a9749d31e715cc21d8d45346888d
Dario Elguero <darioelguero@gmail.com>
2021-08-06 02:54:46 -0300

    Add Users.js and verifyToken.js

---
+++ src/controllers/verifyToken.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { SECRET } = process.env;
+
+function verifyToken(req, res, next ) {
+    const token = req.headers["x-access-token"];
+  if (token) {
+    const decoded = jwt.verify(token, SECRET);
+    if (decoded) {
+      req.userId = decoded.id;
+    } else {
+      return res.status(401).json({ Error: "Token Invalid" });
+    }
+  } else {
+    return res.status(400).json({ Auth: false, message: "No token" });
+  }
+  next();
+}
+
+module.exports = verifyToken;
---
+++ src/users.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+      id: "a63ea909-5f17-416f-8948-c3b421aba319",
+      name: "Dario Elguero",
+      email: "darioelguero@gmail.com",
+      password: "c049cbc3d3752bd2772346d5305fb6f8feb9a948",
+    },
+    {
+      id: "5e8bdb81-8b60-455d-80f3-94804087bfc9",
+      name: "Administrador",
+      email: "admin@gmail.com",
+      password: "e04820372e7f2ebb2d76987433579219b11c2ba5",
+    },
+    {
+      id: "1090406d-3d10-43ea-b683-f71fe65edca0",
+      name: "Jose Pepito",
+      email: "jose@gmail.com",
+      password: "077fe688e3334d2170a3f363b437e7bbc28987b7",
+    },
+  ];
+
+module.exports = users;
